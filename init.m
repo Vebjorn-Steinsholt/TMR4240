f1e0ca1813a7d6795331fec21553d50c7ecbfe39
EvenFosby <125956991+EvenFosby@users.noreply.github.com>
2025-10-14 09:59:32 +0200

    added mass and linear dampning matrix

--- init.m
+++ init.m
@@ -17,3 +17,22 @@ load('supplyABC.mat');
 eta0 = [0,0,0,0,0,0]';
 % Initial velocity u, v, w, p, q, r
 nu0 = [0,0,0,0,0,0]';
+
+
+% LQR Controller
+
+MRB = vessel.MRB;
+MA = vessel.A;
+
+
+% M = MRB + MA;
+
+M = [7.0184e6       0           0;
+        0       8.5464e6    -4.4678e7;
+        0       -4.5028e5   4.0504e9];
+
+D = [2.6486e5       0           0; 
+        0       8.8164e5        0;
+        0           0       3.3774e8];
+
+
