fceb5d35c9854f19be6b1d9661c6c5346a6afd9a
EvenFosby <125956991+EvenFosby@users.noreply.github.com>
2025-11-09 12:27:31 +0100

    some tuning

--- init.m
+++ init.m
@@ -27,10 +27,10 @@ nu0 = [0,0,0,0,0,0]';
 windAngle = 0;
 
 % PID controller
-[Kp, Ki, Kd] = init_pid(vessel);
+[Kp, Ki, Kd, A_eq, B_eq] = init_pid(vessel);
 
 
-t_end = 800;
+t_end = 1200;
 h = 0.1;
 
 %Non-linear Observer
@@ -275,7 +275,7 @@ function [xd,t] = setPointGen(spModeIn, T_final, timeStep)
 spMode = spModeIn; % 1: [0 0 0], 2: single waypoint, 3: multi waypoint, 4: No ref. model
 
 % Position and attitude ref model, based on Fossen, 2021
-T11 = 200; T22 = 300; T33 = 500;
+T11 = 150; T22 = 150; T33 = 250;
 
 omega11 = 2*pi/T11; omega22 = 2*pi/T22; omega33 = 2*pi/T33; % natural freq.
 zeta11 = 1; zeta22 = 1; zeta33 = 1;               % relative damping coeff.
